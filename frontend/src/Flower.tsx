690aa9ee7584e9461d0ab22091075a6ff046283a
Katie Soldau <ksoldau@gmail.com>
2026-08-18 14:29:28 -0400

    flower pic

--- frontend/src/Flower.tsx
+++ frontend/src/Flower.tsx
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 import { useState, useEffect } from "react";
 import flowerNeutral from "../../assets/FlowerNeutral.png";
 import flowerSad from "../../assets/FlowerSad.png";
+=======
+import { useState } from "react";
+import flowerNeutral from "../../assets/FlowerNeutral.png";
+>>>>>>> 91b0da4 (flower pic)
 
 export function Flower() {
   const [mood, _] = useState<"happy" | "sad" | "dead">("happy");
@@ -19,12 +24,19 @@ export function Flower() {
     <div>
       <h1>I'm a flower!</h1>
       <div>
+<<<<<<< HEAD
         I am {mood}
         {mood === "dead" ? (
           <p>DEAD image is pending</p>
         ) : (
           <img src={flowerImg} />
         )}
+=======
+        I am:
+        <br />
+        <h2>{mood}</h2>
+        <img src={flowerNeutral} />
+>>>>>>> 91b0da4 (flower pic)
       </div>
     </div>
   );
