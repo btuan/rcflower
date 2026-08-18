a389ae392d59582963477d0bb63a80ab448a349e
Katie Soldau <ksoldau@gmail.com>
2026-08-18 14:59:45 -0400

    changing flower wip

--- frontend/src/Flower.tsx
+++ frontend/src/Flower.tsx
@@ -1,17 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import flowerNeutral from "../../assets/FlowerNeutral.png";
+import flowerSad from "../../assets/FlowerSad.png";
 
 export function Flower() {
-  const mood = useState<"happy" | "sad" | "dead">("happy");
+  const [mood, _] = useState<"happy" | "sad" | "dead">("happy");
+  const [flowerImg, setFlowerImg] = useState(flowerNeutral);
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (mood === "happy") {
+        setFlowerImg((img) => (img === flowerSad ? flowerNeutral : flowerSad));
+      }
+    }, 2000);
+    return () => clearInterval(id);
+  }, []);
 
   return (
     <div>
       <h1>I'm a flower!</h1>
       <div>
-        I am:
-        <br />
-        <h2>{mood}</h2>
-        <img src={flowerNeutral} />
+        I am {mood}
+        {mood === "dead" ? (
+          <p>DEAD image is pending</p>
+        ) : (
+          <img src={flowerImg} />
+        )}
       </div>
     </div>
   );
