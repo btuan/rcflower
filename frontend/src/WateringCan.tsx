fcbc2c82478ab96611aa79b308ecf257bc950369
Katie Soldau <ksoldau@gmail.com>
2026-09-11 12:02:25 -0400

    test

--- frontend/src/WateringCan.tsx
+++ frontend/src/WateringCan.tsx
@@ -201,6 +201,7 @@ export default function WateringCan() {
       <h1 style={{ fontSize: 22, fontWeight: 500, marginBottom: 16 }}>
         Watering can
       </h1>
+      <p>With this line Katie is testing whether changes showing up on pi</p>
 
       {(() => {
         const src = pouring
