8a2c146772e6f4d2b904d68aac0a8e00298ecec6
Katie Soldau <ksoldau@gmail.com>
2026-09-15 12:42:58 -0400

    remove debugging

--- frontend/src/WateringCan.tsx
+++ frontend/src/WateringCan.tsx
@@ -199,9 +199,8 @@ export default function WateringCan() {
       }}
     >
       <h1 style={{ fontSize: 22, fontWeight: 500, marginBottom: 16 }}>
-        Watering can (TEST)
+        Watering can
       </h1>
-      <p>With this line Katie is testing whether changes showing up on pi</p>
 
       {(() => {
         const src = pouring
